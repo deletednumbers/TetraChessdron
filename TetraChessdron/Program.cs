af58747b2f8dc80c22f4297eed2ffb37d3a63472
Philip Jacobson <phil.jacobson@outlook.com>
2023-04-29 13:41:08 -0700

    new board display, xyz space is now in the class scope

--- TetraChessdron/Program.cs
+++ TetraChessdron/Program.cs
@@ -7,25 +7,33 @@ namespace TetraChessdron
 {
     class TetraChessdron
     {
+        private static List<List<List<string>>> xYZCube = MakeCube();
+
         static void Main(string[] args)
         {
-            List<List<List<string>>> cube = MakeCube();
 
             Console.WriteLine("Hello, World!");
-            PrintPrototypeBoardToConsole();
-            Console.WriteLine(cube[8][8][8]);
+            WriteTetrahedronBoardOntoCube();
+            PrintBoardToConsole();
+
+
+        }
+        private static List<List<List<string>>> WriteTetrahedronBoardOntoCube() 
+        {
+            List<List<List<string>>> tetrahedronInACube = new List<List<List<string>>>();
+            return tetrahedronInACube;
         }
 
         private static List<List<List<string>>> MakeCube()
         {
-            List<List<List<string>>> board = new List<List<List<string>>>();
+            List<List<List<string>>> cube = new List<List<List<string>>>();
 
             for (var index = 0; index < 9; index++)
             {
-                board.Add(MakePlaneOfCube());
+                cube.Add(MakePlaneOfCube());
             }
 
-            return board;
+            return cube;
         }
         private static List<List<string>> MakePlaneOfCube()
         {
@@ -50,7 +58,7 @@ namespace TetraChessdron
 
             return row;
         }
-        class CubeCell 
+        class CubeCell
         {
             private static string anEmptyCell = "";
             public string EmptyCubeCell()
@@ -59,53 +67,63 @@ namespace TetraChessdron
             }
 
         }
-        /*
+        class TetrahedronCell
+        {
+            private static string anEmptyCell = "-0-";
+            public string EmptyTetrahedronCell()
+            {
+                return anEmptyCell;
+            }
+
+        }
+        
         private static void PrintBoardToConsole()
         {
+
             Console.WriteLine($"|---|---|---|---|---|---|---|---|");
-            Console.WriteLine($"|{G188}|{G287}|{G386}|{G485}|{G584}|{G683}|{G782}|{G881}|");
+            Console.WriteLine($"|"+xYZCube[1][8][8]+"|"+xYZCube[2][8][7]+"|"+xYZCube[3][8][6]+"|"+xYZCube[4][8][5]+"|"+xYZCube[5][8][4]+"|"+xYZCube[6][8][3]+"|"+xYZCube[7][8][2]+"|"+xYZCube[8][8][1]+"|");
             Console.WriteLine($"|---|---|---|---|---|---|---|---|");
-            Console.WriteLine($"    |{G276}|{G375}|{G474}|{G573}|{G672}|{G771}|{G872}|");
+            Console.WriteLine($"    |"+xYZCube[2][7][6]+"|"+xYZCube[3][7][5]+"|"+xYZCube[4][7][4]+"|"+xYZCube[5][7][3]+"|"+xYZCube[6][7][2]+"|"+xYZCube[7][7][1]+"|"+xYZCube[8][7][2]+"|");
             Console.WriteLine($"|---|---|---|---|---|---|---|---|");
-            Console.WriteLine($"|{G177}|{G278}|{G377}|{G476}|{G575}|{G675}|{G773}|");
+            Console.WriteLine($"|"+xYZCube[1][7][7]+"|"+xYZCube[2][7][8]+"|"+xYZCube[3][7][7]+"|"+xYZCube[4][7][6]+"|"+xYZCube[5][7][5]+"|"+xYZCube[6][7][5]+"|"+xYZCube[7][7][3]+"|");
             Console.WriteLine($"|---|---|---|---|---|---|---|---|");
-            Console.WriteLine($"        |{G364}|{G463}|{G562}|{G661}|{G761}|{G863}|");
+            Console.WriteLine($"        |"+xYZCube[3][6][4]+"|"+xYZCube[4][6][3]+"|"+xYZCube[5][6][2]+"|"+xYZCube[6][6][1]+"|"+xYZCube[7][6][1]+"|"+xYZCube[8][6][3]+"|");
             Console.WriteLine($"    |---|---|---|---|---|---|---|");
-            Console.WriteLine($"    |{G265}|{G366}|{G465}|{G564}|{G663}|{G764}|");
+            Console.WriteLine($"    |"+xYZCube[2][6][5]+"|"+xYZCube[3][6][6]+"|"+xYZCube[4][6][5]+"|"+xYZCube[5][6][4]+"|"+xYZCube[6][6][3]+"|"+xYZCube[7][6][4]+"|");
             Console.WriteLine($"|---|---|---|---|---|---|---|");
-            Console.WriteLine($"|{G166}|{G267}|{G368}|{G467}|{G566}|{G665}|");
+            Console.WriteLine($"|"+xYZCube[1][6][6]+"|"+xYZCube[2][6][7]+"|"+xYZCube[3][6][8]+"|"+xYZCube[4][6][7]+"|"+xYZCube[5][6][6]+"|"+xYZCube[6][6][5]+"|");
             Console.WriteLine($"|---|---|---|---|---|---|---|---|");
-            Console.WriteLine($"            |{G452}|{G551}|{G652}|{G753}|{G854}|");
+            Console.WriteLine($"            |"+xYZCube[4][5][2]+"|"+xYZCube[5][5][1]+"|"+xYZCube[6][5][2]+"|"+xYZCube[7][5][3]+"|"+xYZCube[8][5][4]+"|");
             Console.WriteLine($"        |---|---|---|---|---|---|");
-            Console.WriteLine($"        |{G353}|{G454}|{G553}|{G654}|{G755}|");
+            Console.WriteLine($"        |"+xYZCube[3][5][3]+"|"+xYZCube[4][5][4]+"|"+xYZCube[5][5][3]+"|"+xYZCube[6][5][4]+"|"+xYZCube[7][5][5]+"|");
             Console.WriteLine($"    |---|---|---|---|---|---|");
-            Console.WriteLine($"    |{G254}|{G355}|{G456}|{G555}|{G656}|");
+            Console.WriteLine($"    |"+xYZCube[2][5][4]+"|"+xYZCube[3][5][5]+"|"+xYZCube[4][5][6]+"|"+xYZCube[5][5][5]+"|"+xYZCube[6][5][6]+"|");
             Console.WriteLine($"|---|---|---|---|---|---|");
-            Console.WriteLine($"|{G155}|{G256}|{G357}|{G458}|{G557}|");
+            Console.WriteLine($"|"+xYZCube[1][5][5]+"|"+xYZCube[2][5][6]+"|"+xYZCube[3][5][7]+"|"+xYZCube[4][5][8]+"|"+xYZCube[5][5][7]+"|");
             Console.WriteLine($"|---|---|---|---|---|---|---|---|");
-            Console.WriteLine($"            |{G441}|{G542}|{G643}|{G744}|{G845}|");
+            Console.WriteLine($"            |"+xYZCube[4][4][1]+"|"+xYZCube[5][4][2]+"|"+xYZCube[6][4][3]+"|"+xYZCube[7][4][4]+"|"+xYZCube[8][4][5]+"|");
             Console.WriteLine($"        |---|---|---|---|---|---|");
-            Console.WriteLine($"        |{G342}|{G443}|{G544}|{G645}|{G746}|");
+            Console.WriteLine($"        |"+xYZCube[3][4][2]+"|"+xYZCube[4][4][3]+"|"+xYZCube[5][4][4]+"|"+xYZCube[6][4][5]+"|"+xYZCube[7][4][6]+"|");
             Console.WriteLine($"        |---|---|---|---|---|");
-            Console.WriteLine($"    |{G243}|{G344}|{G445}|{G546}|{G647}|");
+            Console.WriteLine($"    |"+xYZCube[2][4][3]+"|"+xYZCube[3][4][4]+"|"+xYZCube[4][4][5]+"|"+xYZCube[5][4][6]+"|"+xYZCube[6][4][7]+"|");
             Console.WriteLine($"|---|---|---|---|---|---|");
-            Console.WriteLine($"|{G144}|{G245}|{G346}|{G447}|{G548}|");
+            Console.WriteLine($"|"+xYZCube[1][4][4]+"|"+xYZCube[2][4][5]+"|"+xYZCube[3][4][6]+"|"+xYZCube[4][4][7]+"|"+xYZCube[5][4][8]+"|");
             Console.WriteLine($"|---|---|---|---|---|---|");
-            Console.WriteLine($"        |{G331}|{G432}|{G533}|{G634}|{G735}|{G836}|");
+            Console.WriteLine($"        |"+xYZCube[3][3][1]+"|"+xYZCube[4][3][2]+"|"+xYZCube[5][3][3]+"|"+xYZCube[6][3][4]+"|"+xYZCube[7][3][5]+"|"+xYZCube[8][3][6]+"|");
             Console.WriteLine($"        |---|---|---|---|---|---|");
-            Console.WriteLine($"    |{G232}|{G333}|{G434}|{G535}|{G636}|{G737}|");
+            Console.WriteLine($"    |"+xYZCube[2][3][2]+"|"+xYZCube[3][3][3]+"|"+xYZCube[4][3][4]+"|"+xYZCube[5][3][5]+"|"+xYZCube[6][3][6]+"|"+xYZCube[7][3][7]+"|");
             Console.WriteLine($"|---|---|---|---|---|---|---|");
-            Console.WriteLine($"|{G133}|{G234}|{G335}|{G436}|{G537}|{G638}|");
+            Console.WriteLine($"|"+xYZCube[1][3][3]+"|"+xYZCube[2][3][4]+"|"+xYZCube[3][3][5]+"|"+xYZCube[4][3][6]+"|"+xYZCube[5][3][7]+"|"+xYZCube[6][3][8]+"|");
             Console.WriteLine($"|---|---|---|---|---|---|---|---|");
-            Console.WriteLine($"    |{G221}|{G322}|{G423}|{G524}|{G625}|{G726}|{G827}|");
+            Console.WriteLine($"    |"+xYZCube[2][2][1]+"|"+xYZCube[3][2][2]+"|"+xYZCube[4][2][3]+"|"+xYZCube[5][2][4]+"|"+xYZCube[6][2][5]+"|"+xYZCube[7][2][6]+"|"+xYZCube[8][2][7]+"|");
             Console.WriteLine($"|---|---|---|---|---|---|---|---|");
-            Console.WriteLine($"|{G122}|{G223}|{G324}|{G425}|{G526}|{G627}|{G728}|");
+            Console.WriteLine($"|"+xYZCube[1][2][2]+"|"+xYZCube[2][2][3]+"|"+xYZCube[3][2][4]+"|"+xYZCube[4][2][5]+"|"+xYZCube[5][2][6]+"|"+xYZCube[6][2][7]+"|"+xYZCube[7][2][8]+"|");
             Console.WriteLine($"|---|---|---|---|---|---|---|---|");
-            Console.WriteLine($"|{G111}|{G212}|{G313}|{G414}|{G515}|{G616}|{G717}|{G818}|");
+            Console.WriteLine($"|"+xYZCube[1][1][1]+"|"+xYZCube[2][1][2]+"|"+xYZCube[3][1][3]+"|"+xYZCube[4][1][4]+"|"+xYZCube[5][1][5]+"|"+xYZCube[6][1][6]+"|"+xYZCube[7][1][7]+"|"+xYZCube[8][1][8]+"|");
             Console.WriteLine($"|---|---|---|---|---|---|---|---|");
             Console.WriteLine("");
         }
-        */
+        
         private static void PrintPrototypeBoardToConsole()
         {
 
